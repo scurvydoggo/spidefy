fd06fb891df6a8e7056d163315c7ad7ff7223245
Andreas Nygard <andreas.nygard@gmail.com>
2025-02-28 14:59:10 +1100

    hotkey: Add example

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,7 +7,24 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_global_shortcut::Builder::new()
+            .with_shortcuts(["ctrl+d", "alt+space"]).expect("Shortcut syntax error")
+            .with_handler(|app, shortcut, event| {
+                use tauri::Emitter;
+                use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState};
+                if event.state == ShortcutState::Pressed {
+                    if shortcut.matches(Modifiers::CONTROL, Code::KeyD) {
+                        let _ = app.emit("shortcut-event", "Ctrl+D triggered");
+                        print!("Ctrl+D");
+                    }
+                    if shortcut.matches(Modifiers::ALT, Code::Space) {
+                        let _ = app.emit("shortcut-event", "Alt+Space triggered");
+                        print!("Alt+Space!");
+                    }
+                }
+            })
+            .build()
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
